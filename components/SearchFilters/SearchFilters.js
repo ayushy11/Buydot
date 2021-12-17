ce9b9e4f2c486899a8adac8a0148d3c255a2ca48
AYUSH YADAV <ayushexx@gmail.com>
2021-12-17 13:23:21 +0530

    feat: search page

---
+++ components/SearchFilters/SearchFilters.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import {
+  Box,
+  Flex,
+  Text,
+  Icon,
+  Select,
+  Input,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
+import { MdCancel } from "react-icons/md";
+
+const SearchFilters = () => {
+    return (
+        <Box>
+            ....
+        </Box>
+    )
+}
+
+export default SearchFilters;
